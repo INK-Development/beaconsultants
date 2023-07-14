65458c04fb74321f83d9af24a597969308f26f19
nickS1337 <nick.petjaten@gmail.com>
2023-07-14 22:23:21 +1000

    completed minimum viable product

--- public/solutions/list.js
+++ public/solutions/list.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         console.log(res);
         for (var i = 0; i < res.length; i++){
             $("#section-modules").append(`
-            <a href="/services/modules/?id=`+ res[i].id + `">
+            <a href="/solutions/modules/?id=`+ res[i].id + `">
                 <div class="module">
                     <div class="module-icon"></div>
                     <div class="module-text">`+res[i].name+`</div>
