3b486ef2597ac374e09e0b5b5d8a93600c247542
nickS1337 <nick.petjaten@gmail.com>
2023-09-13 15:34:11 +1000

    fixed typos on index.html

--- public/companies.js
+++ public/companies.js
@@ -15,55 +15,55 @@ let timeout = 5000;
 //The number that is showing on the viewport. For example, if #crow-1B is showing this will be 1 (position in alphabet[])
 let viewport1 = 0;
 
-// document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", ()=>{
 
-//     setInterval(()=>{
+    setInterval(()=>{
 
-//         //If #crow-1A is currently showing
-//         if (viewport1 == 0){
+        //If #crow-1A is currently showing
+        if (viewport1 == 0){
 
-//             //Move #crow-1A down:
-//             $("#crow-1A").animate({ "top": "-100%" });
+            //Move #crow-1A down:
+            $("#crow-1A").animate({ "top": "-100%" });
 
-//             //Move #crow-1B  up:
-//             $("#crow-1B").animate({ "top": "0%" });
+            //Move #crow-1B  up:
+            $("#crow-1B").animate({ "top": "0%" });
 
-//             //Now populate the images for each item in #crow-1B:
-//             for (var i = 1; i < 5; i++){
-//                 let image = images[Math.floor(Math.random() * images.length)+1];
-//                 console.log("adding in " + " #crow-1B_" + i + ", /pics/companies/" + image);
-//                 changeBg("crow-1B_" + i, image);
-//             }
+            //Now populate the images for each item in #crow-1B:
+            for (var i = 1; i < 5; i++){
+                let image = images[Math.floor(Math.random() * images.length)+1];
+                console.log("adding in " + " #crow-1B_" + i + ", /pics/companies/" + image);
+                changeBg("crow-1B_" + i, image);
+            }
 
-//             //Change the viewport for next time:
-//             viewport1 = 1;
+            //Change the viewport for next time:
+            viewport1 = 1;
 
-//         } else {
-//             //Otherwise, #cow-1B is currently showing
+        } else {
+            //Otherwise, #cow-1B is currently showing
 
-//             //Move #crow-1A up:
-//             $("#crow-1A").animate({ "top": "0%" });
+            //Move #crow-1A up:
+            $("#crow-1A").animate({ "top": "0%" });
 
-//             //Move #crow-1B down:
-//             $("#crow-1B").animate({ "top": "100%" });
+            //Move #crow-1B down:
+            $("#crow-1B").animate({ "top": "100%" });
 
-//             //Now populate the images for each item in #crow-1A:
-//             for (var i = 1; i < 5; i++){
-//                 let image = images[Math.floor(Math.random() * images.length)+1];
-//                 console.log("adding in " + " #crow-1A_" + i + ", /pics/companies/" + image);
-//                 changeBg("crow-1A_" + i, image);
-//             }
+            //Now populate the images for each item in #crow-1A:
+            for (var i = 1; i < 5; i++){
+                let image = images[Math.floor(Math.random() * images.length)+1];
+                console.log("adding in " + " #crow-1A_" + i + ", /pics/companies/" + image);
+                changeBg("crow-1A_" + i, image);
+            }
 
-//             //Change the viewport for next time:
-//             viewport1 = 0;
+            //Change the viewport for next time:
+            viewport1 = 0;
 
-//         }
+        }
 
-//     }, 5000);
+    }, 5000);
 
-// });
+});
 
 function changeBg(id, image){
     console.log("id: " + id, ", image: " + image);
-    document.getElementById(id).style.backgroundImage = "url('/pics/companies/" + image + "');";
+    document.getElementById(id).style.backgroundImage = "url('" + image + "');";
 }
