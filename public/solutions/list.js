c4c6f2225463cee29c5123d4b52a9c03d03a2805
nickS1337 <nick.petjaten@gmail.com>
2023-09-18 00:26:21 +1000

    added modules icon to modules page

--- public/solutions/list.js
+++ public/solutions/list.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         for (var i = 0; i < res.length; i++){
             $("#section-modules").append(`
             <a href="/solutions/modules/?id=`+ res[i].id + `">
-                <div class="module">
+                <div class="module" style="background-image: url('/pics/modules/`+res[i].id+`.png');">
                     <div class="module-icon"></div>
                     <div class="module-text">`+res[i].name+`</div>
                 </div>
